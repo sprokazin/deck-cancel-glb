bc1dd119df4d556777d3f26489bbdf75d9cf7c9d
psa <psa@cgis.io>
2023-07-04 11:46:13 +0200

    added glb model

--- src/components/layers/ScenegraphLayer/ScenegraphLayerToCancel.js
+++ src/components/layers/ScenegraphLayer/ScenegraphLayerToCancel.js
@@ -2,8 +2,9 @@ import React from 'react';
 import {ScenegraphLayer} from "deck.gl";
 import {GLTFLoader} from '@loaders.gl/gltf';
 
+
 const data = [{name: 'Colma (COLM)', address: '365 D Street, Colma CA 94014', exits: 4214, coordinates: [-122.466233, 37.684638]}]
-const url = 'https://minio.cgis.io/cubagis-gallery/1bfbd0bd-4133-6917-6ab5-0fc4ee8fcb58--pavilon_%4034_2.glb'
+const url = 'https://files.kaiten.ru/b49de011-4884-4a23-873b-d941e3f659ab.glb?name=futuristic_building.glb'
 
 const ScenegraphLayerToCancel = (signal) => {
 
@@ -13,14 +14,15 @@ const ScenegraphLayerToCancel = (signal) => {
            scenegraph: url,
            pickable: true,
            getPosition: d => d.coordinates,
-           getOrientation: d => [0, 0, 90],
+           getOrientation: d => [0, -45, 90],
            loadOptions: {
                fetch: {
                    signal: signal
                }
            },
            loaders: [GLTFLoader],
-           _lighting: 'pbr'
+           _lighting: 'pbr',
+           sizeScale: 5
        })
     ]
 };
